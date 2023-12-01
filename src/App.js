6f15ae15f307c8ee20d7ff3f7a98af0ffd183e4c
Dias123123 <tianshanbekdias@gmail.com>
2023-12-01 21:54:03 +0600

    сделал красный и синий квадрат

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from "react";
-import './App.css';
+import React, {useState} from "react";
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <h1>apsjfoiasfjeiofef</h1>
-    </div>
-  );
+    // Квадраты
+    const [isBlue, setIsBlue] = useState(false)
+    const handleColorChange = () => {
+        setIsBlue(!isBlue)
+    }
+
+    // Todo список
+    const [todoList, setTodoList] = useState([])
+    const [inputValue, setInputValue] = useState("")
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value)
+    }
+
+    const handleAddTask = () => {
+        setTodoList([...todoList, inputValue])
+    }
+
+
+    return (
+        <div className="App">
+            <div onClick={handleColorChange} className={`${isBlue ? "blue" : "red"} cube`}></div>
+            <div onClick={handleColorChange} className={`${!isBlue ? "blue" : "red"} cube`}></div>
+            <input onChange={handleInputChange} value={inputValue} type="text"/>
+            <button onClick={handleAddTask}>Добавить в список</button>
+
+            {todoList.map((item, index) => (
+                <p key={index}>{item}</p>
+            ))}
+        </div>
+    );
 }
 
 export default App;
